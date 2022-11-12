7f0a41cb1f32a26e02baa9616084073267c28fb2
Alexandre Negrel <negrel.dev@protonmail.com>
2022-11-12 21:42:05 +0000

    feat: add security to list_removed script

--- scripts/list_removed.sh
+++ scripts/list_removed.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+FORCE="${FORCE:=n}"
 MAX_JOB="${MAX_JOB:=256}"
 ulimit -u "$MAX_JOB"
 
@@ -14,6 +15,10 @@ list_removed() {
 		elif [ -L "$f" ]; then
 			stat -L "$f" &> /dev/null && echo "$f"
 		elif [ -d "$f" ]; then
+			if [[ "${f,,}" =~ .*"sync".* && "$FORCE" != "y" ]]; then
+				echo "skipping directory \"$f\" because it contains \"sync\" in its path, rerun with FORCE=y to override." >&2
+				continue
+			fi
 			list_removed "$f"/* &
 		fi
 	done
